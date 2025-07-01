7a399e47ed1a2542fae3533ccba36a33607944b3
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-07-01 15:05:55 +0200

    Enhance form accessibility by adding ARIA attributes and improving error handling for email and message fields
    We are not done yet. Accessibility should never be overlooked. Let's make the form more accessible by adding the following attributes:
    Make sure each label is associated with the input properly with an unique id
    Setup validation attributes similar to the zod schema
    Configure the aria-invalid attribute of the form elements based on the validity
    Make sure the error message is linked to a form element with the aria-describedby attribute

--- playground/app/routes/parse-with-effect-schema.tsx
+++ playground/app/routes/parse-with-effect-schema.tsx
@@ -106,29 +106,51 @@ export async function action({ request }: ActionFunctionArgs) {
 	}
 
 	// return redirect('/messages')
+	return null;
 }
 
 export default function Example() {
 	const result = useActionData<typeof action>();
 
 	return (
-		<Form method="post">
-			<div>{result?.formErrors}</div>
+		<Form
+			method="post"
+			aria-describedby={result?.formErrors ? 'contact-error' : undefined}
+		>
+			<div id="contact-error">{result?.formErrors}</div>
 
 			<Playground title="Mutliple Errors" result={result}>
 				<div>
-					<label>Email</label>
+					<label htmlFor="contact-email">Email</label>
 					<input
+						id="contact-email"
 						type="email"
 						name="email"
 						defaultValue={result?.payload.email}
+						required
+						aria-invalid={result?.fieldErrors.email ? true : undefined}
+						aria-describedby={
+							result?.fieldErrors.email ? 'contact-email-error' : undefined
+						}
 					/>
-					<div>{result?.fieldErrors.email}</div>
+					<div id="contact-email-error">{result?.fieldErrors.email}</div>
 				</div>
+
 				<div>
-					<label>Message</label>
-					<textarea name="message" defaultValue={result?.payload.message} />
-					<div>{result?.fieldErrors.message}</div>
+					<label htmlFor="contact-message">Message</label>
+					<textarea
+						id="contact-message"
+						name="message"
+						defaultValue={result?.payload.message}
+						required
+						minLength={10}
+						maxLength={100}
+						aria-invalid={result?.fieldErrors.message ? true : undefined}
+						aria-describedby={
+							result?.fieldErrors.message ? 'contact-email-message' : undefined
+						}
+					/>
+					<div id="contact-email-message">{result?.fieldErrors.message}</div>
 				</div>
 			</Playground>
 		</Form>
