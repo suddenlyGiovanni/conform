34e8afd98f956dcade20ab6953c8834d62e4945b
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-07-01 23:38:00 +0200

    Replace `zod` with `effect` in dependencies and remove associated files and utilities from `@conform-to/effect-schema` package.

--- packages/conform-effect-schema/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { getZodConstraint } from './constraint';
-export { parseWithZod, conformZodMessage } from './parse';
-export { coerceFormValue as unstable_coerceFormValue } from './coercion';
